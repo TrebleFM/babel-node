d7ac5900ba023e6bd7dc0a6b2258bae6da6f57da
Spencer Hakim <spencer@treble.fm>
2016-11-04 12:53:52 -0400

    Little bit of test work

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,10 @@
-// TODO - Actually write some decent tests
+/*
+ * TODO - Actually write some decent tests
+ *
+ * Empty .babelrc makes sure configs are merged, not overwritten
+ */
+
+import "module";
+
 console.log("process.argv =", process.argv);
 console.log("module =", module);
